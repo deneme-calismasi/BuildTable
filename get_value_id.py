08a50c2931479c7579cfef6b99f6e16419743d37
halilerhan.orun <erhan0494@gmail.com>
2021-09-05 15:10:03 +0300

    05.09.2021 15:09

---
+++ get_value_id.py
@@ -0,0 +1,29 @@
+from tkinter import *
+import tkinter.ttk as ttk
+
+root = Tk()
+
+
+def OnDoubleClick(event):
+    print(tree.identify_row(event.y))
+
+
+tree = ttk.Treeview(root)
+
+tree["columns"] = ("one", "two")
+tree.heading("one", text="coulmn A")
+tree.heading("two", text="column B")
+
+tree.insert("", 3, "dir3", text="Dir 3", values=("3A", " 3B"))
+# tree.insert("dir3", 3, 'subdir3', text="sub dir 3", values=("3A", " 3B"))
+
+for item in tree.selection():
+    item_child = tree.get_children(item)
+    item = tree.item(item)
+    iid = tree.focus()
+    print("iid = ", iid, "Item = ", item, "Child iid's =", item_child)
+
+tree.bind("<Double-1>", OnDoubleClick)
+
+tree.pack()
+root.mainloop()
---
+++ item_using_id.py
@@ -0,0 +1,24 @@
+from tkinter import *
+import tkinter.ttk as ttk
+
+root = Tk()
+
+
+def OnDoubleClick(event):
+    item = tree.identify("item", event.x, event.y)
+    print("you clicked on", tree.item(item)["text"])
+
+
+tree = ttk.Treeview(root)
+
+tree["columns"] = ("one", "two")
+tree.heading("one", text="coulmn A")
+tree.heading("two", text="column B")
+
+tree.insert("", 3, "dir3", text="Dir 3", values=("3A", " 3B"))
+tree.insert("dir3", 3, 'subdir3', text="sub dir 3", values=("3A", " 3B"))
+
+tree.bind("<Double-1>", OnDoubleClick)
+
+tree.pack()
+root.mainloop()
